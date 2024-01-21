35e243400aaf59b5678c2629fc3528a8b2a1a7f5
andnikis <andreymon@gmail.com>
2024-01-22 00:29:09 +0600

    refactor: change 03-files-in-folder output message format and remove const readdir

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,15 +1,14 @@
 const fs = require('fs').promises;
-const { readdir } = require('fs').promises;
 const path = require('path');
 
 async function printFilesInformation(dirName) {
-  const items = await readdir(dirName, { withFileTypes: true });
+  const items = await fs.readdir(dirName, { withFileTypes: true });
 
   for (const item of items) {
     if (item.isFile()) {
       const stats = await fs.stat(path.join(dirName, item.name));
       console.log(
-        `${item.name} - ${path.extname(item.name).slice(1)} - ${stats.size}`,
+        `${item.name} - ${path.extname(item.name).slice(1)} - ${stats.size}b`,
       );
     }
   }
